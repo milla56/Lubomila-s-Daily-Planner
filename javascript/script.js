9e9b37600555df89259e0ff67da09d88905017d6
milla56 <lubomila14@gmail.com>
2023-04-28 19:09:46 +0100

    current date and time added to kavascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,12 @@
+var containerId = $("#container");
 
 // Current Day
-$("#currentDay").text(now.format("dddd MMMM DD, YYYY"));
+$(document).ready(function (){
+  let nowMoment = moment().format("MMMM Do YYYY");
+  let currentDay = $("#currentDay");
+  displayDate.innerHTML = nowMoment;
+  let currentHour = moment().format("HH");
+})
 
+
+// time slot compared to actual the actual times
