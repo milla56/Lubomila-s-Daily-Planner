a5e1e79cdd65d1d6b23d7be7533d068b54829766
milla56 <lubomila14@gmail.com>
2023-04-28 19:56:45 +0100

    time slot function added to js and tr tag added to html

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,41 @@
 var containerId = $("#container");
 
 // Current Day
-$(document).ready(function (){
-  let nowMoment = moment().format("MMMM Do YYYY");
-  let currentDay = $("#currentDay");
-  displayDate.innerHTML = nowMoment;
-  let currentHour = moment().format("HH");
-})
+// $(document).ready(function (){
+//   let nowMoment = moment().format("MMMM Do YYYY");
+//   let currentDay = $("#currentDay");
+//   currentDay.innerHTML = nowMoment;
+//   let currentHour = moment().format("HH");
+// })
+
+$(document).ready(function(){
+	const currentDay = moment().format('dddd, MMMM Do YYYY');
+	$('#currentDay').html(currentDay);})
 
 
 // time slot compared to actual the actual times
+let currentHour = moment().format("HH");
+console.log(currentHour);
+
+$(".calendar").each(function(){
+  var timeDiv = $(this).find(".time-div").attr("id"); //time element
+  var description = $(this).find(".description");     // text area
+  if (currentHour === timeDiv){
+    description.addClass("present").css ("background-color", "#ff6961");
+  }
+  else if (currentHour > timeDiv){
+    description.addClass("future").css("background-color","#77dd77");
+  }
+else (currentHour < timeDiv);{
+  description.addClass("past").css("background-color" ,"#d3d3d3")
+}
+})
+
+
+
+
+
+
+ 
+
+
