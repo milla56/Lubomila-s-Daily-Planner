3b0d3f98dd9458bb1d5a0d32e18a2f2ad81b5bd7
milla56 <lubomila14@gmail.com>
2023-04-29 18:17:16 +0100

    local storage added to js and new hours to html

--- javascript/script.js
+++ javascript/script.js
@@ -49,7 +49,28 @@ saveBtn.on("click",function(event){
 
 
 
-//events on local storage
+//get items from local storage
+$("#hour-09 .description").val(localStorage.getItem("hour-09"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+$("#hour-18 .description").val(localStorage.getItem("hour-18"));
+$("#hour-19 .description").val(localStorage.getItem("hour-19"));
+
+
+
+
+
+
+
+
+
+
 
  
 
