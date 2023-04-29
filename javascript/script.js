a82da6186149a8209b1de08a9b44f3d08316e023
milla56 <lubomila14@gmail.com>
2023-04-29 18:39:21 +0100

    fixing mistakes on code

--- javascript/script.js
+++ javascript/script.js
@@ -3,53 +3,73 @@ var saveBtn = $('.save button');
 var timeDiv = $(".time-div"); // time element
 var description = $(".description"); //text area
 
+const currentDay = moment().format('dddd, MMMM Do YYYY');
+	$('#currentDay').html(currentDay);
+
 // Current Day
 $(document).ready(function(){
-	const currentDay = moment().format('dddd, MMMM Do YYYY');
-	$('#currentDay').html(currentDay);})
+	// const currentDay = moment().format('dddd, MMMM Do YYYY');
+	// $('#currentDay').html(currentDay);
+
+  saveBtn.on("click",function(event){
+    event.preventDefault();
+   
+    var textInput = $(this).siblings(".description").val();
+    var time = $(this).parent().attr('id');
+  
+    localStorage.setItem(time,textInput)
+    
+  }) 
+})
 
 
 // time slot compared to actual the actual times
-let currentHour = moment().format("HH");
-console.log(currentHour);
+// let currentHour = moment().format("HH");
+// console.log(currentHour);
 
-timeDiv.each(function(){
+$(".time-div").each(function(){
+  // let currentHour = moment().format("HH");
+  // console.log(currentHour);
+var currentHour = moment().hour();
+
   var timeElement = parseInt($(this).attr('id').split("hour")[1]);
-  if (currentHour === timeDiv){
-    description.addClass("present");
+
+  if (timeElement === currentHour){
     description.removeClass("future");
     description.removeClass("past");
+    description.addClass("present");
   }
-  else if (currentHour > timeDiv){
-    description.addClass("future");
+  else if (timeElement > currentHour){
     description.removeClass("present");
     description.removeClass("past");
+    description.addClass("future");
   }
-else (currentHour < timeDiv);{
-  description.addClass("past");
+else (timeElement < currentHour);{
   description.removeClass("present");
   description.removeClass("future");
+  description.addClass("past");
 }
 })
 
 
 // save btn
 
-saveBtn.on("click",function(event){
-  event.preventDefault();
+// saveBtn.on("click",function(event){
+//   event.preventDefault();
  
-  var textInput = $(this).siblings(".description").val();
-  var time = $(this).parent().attr('id');
+//   var textInput = $(this).siblings(".description").val();
+//   var time = $(this).parent().attr('id');
 
-  localStorage.setItem(time,textInput)
+//   localStorage.setItem(time,textInput)
   
-}) 
+// }) 
   
 //remove btn ?
 
 
 
 //get items from local storage
+
 $("#hour-09 .description").val(localStorage.getItem("hour-09"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
 $("#hour-11 .description").val(localStorage.getItem("hour-11"));
@@ -62,6 +82,7 @@ $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 $("#hour-18 .description").val(localStorage.getItem("hour-18"));
 $("#hour-19 .description").val(localStorage.getItem("hour-19"));
 
+timeDiv();
 
 
 
